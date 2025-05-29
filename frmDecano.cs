bc55bcc8766f1be5614856bff2d519b3e67ba108
Armando Villamil <119384839+Arvillax@users.noreply.github.com>
2025-05-28 18:57:55 -0600

    Revert "procedimientos almacenados movidos a la clase"
    This reverts commit eb87c674e0beeaadebff94265028852237af22b6.

--- frmDecano.cs
+++ frmDecano.cs
@@ -13,7 +13,7 @@ namespace Proyecto_DesarrolloSoftware
 {
     public partial class frmDecano : Form
     {
-        
+        string server = "workstation id=ProyectoFinal.mssql.somee.com;packet size=4096;user id=JRivera_SQLLogin_1;pwd=cokdua1z5a;data source=ProyectoFinal.mssql.somee.com;persist security info=False;initial catalog=ProyectoFinal;TrustServerCertificate=True";
         SqlConnection conectar = new SqlConnection();
         clsConexion con = new clsConexion();
 
@@ -36,6 +36,21 @@ namespace Proyecto_DesarrolloSoftware
         {
             int usu_decano = Convert.ToInt32(txt_usu_decano.Text);
 
+            conectar.ConnectionString = server;
+            conectar.Open();
+            SqlCommand cmd = new SqlCommand("sp_mos_decano", conectar);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@id_decano", usu_decano);
+
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+                throw;
+            }
             con.visualizar_decano(usu_decano, dataGridView1);
             conectar.Close();
         }
@@ -45,9 +60,110 @@ namespace Proyecto_DesarrolloSoftware
             
         }
 
+        private void hidesubmenu()
+        {
+            
+        }
+
+        private void showsubmenu(Panel submenu)
+        {
+            if (submenu.Visible == false)
+            {
+                hidesubmenu();
+                submenu.Visible = true;
+            }
+            else
+                submenu.Visible = false;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_most_sem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btn_cierre_Click(object sender, EventArgs e)
+        {
+            Form1 frmLogin = new Form1();
+            frmLogin.Show();
+            this.Close();
+        }
+
+        private void btn_most_sem_Click_1(object sender, EventArgs e)
+        {
+           
+        }
+
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             txt_idasis.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+        }
+
+        private void rdb_observacion_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void rdb_justificado_CheckedChanged(object sender, EventArgs e)
+        {
+            string est_asis = "R";
+            int id_asistencia = Convert.ToInt32(txt_idasis.Text);
+            int id_decano = Convert.ToInt32(txt_usu_decano.Text);
+            
+            
+            conectar.ConnectionString = server;
+            conectar.Open();
+
+            SqlCommand cmd = new SqlCommand("sp_justificacion ", conectar);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@estado_justificacion", est_asis);
+            cmd.Parameters.AddWithValue("@id_asistencia", id_asistencia);
+            cmd.Parameters.AddWithValue("@id_decano", id_decano);
+
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+                throw;
+            }
+            con.marcar_justificacion(est_asis, id_asistencia, id_decano, dataGridView1);
+            conectar.Close();
+        }
+
+        private void rdb_no_justificado_CheckedChanged(object sender, EventArgs e)
+        {
+            string est_asis = "N";
+            int id_asistencia = Convert.ToInt32(txt_idasis.Text);
+            int id_decano = Convert.ToInt32(txt_usu_decano.Text);
+
+            conectar.ConnectionString = server;
+            conectar.Open();
+            
+
+            SqlCommand cmd = new SqlCommand("sp_justificacion ", conectar);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@estado_justificacion", est_asis);
+            cmd.Parameters.AddWithValue("@id_asistencia", id_asistencia);
+            cmd.Parameters.AddWithValue("@id_decano", id_decano);
+
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+                throw;
+            }
+            con.marcar_justificacion(est_asis, id_asistencia, id_decano, dataGridView1);
+            conectar.Close();
         }
 
         private void btn_cierre_sesion_Click(object sender, EventArgs e)
@@ -59,41 +175,66 @@ namespace Proyecto_DesarrolloSoftware
 
         private void dataGridView1_SelectionChanged(object sender, EventArgs e)
         {
-          
+           // rdb_justificado.Checked = false;
+           // rdb_no_justificado.Checked = false;
         }
 
         private void btn_justificado_Click(object sender, EventArgs e)
         {
-            if (string.IsNullOrEmpty(txt_idasis.Text))
-            {
-                MessageBox.Show("Escoja un registro para marcar la Justificación");
-            }
-            else
-            {
-                string est_asis = "R";
-                int id_asistencia = Convert.ToInt32(txt_idasis.Text);
-                int id_decano = Convert.ToInt32(txt_usu_decano.Text);
+            string est_asis = "R";
+            int id_asistencia = Convert.ToInt32(txt_idasis.Text);
+            int id_decano = Convert.ToInt32(txt_usu_decano.Text);
 
-                con.marcar_justificacion(est_asis, id_asistencia, id_decano, dataGridView1);
-                conectar.Close();
+
+            conectar.ConnectionString = server;
+            conectar.Open();
+
+            SqlCommand cmd = new SqlCommand("sp_justificacion ", conectar);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@estado_justificacion", est_asis);
+            cmd.Parameters.AddWithValue("@id_asistencia", id_asistencia);
+            cmd.Parameters.AddWithValue("@id_decano", id_decano);
+
+            try
+            {
+                cmd.ExecuteNonQuery();
             }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+                throw;
+            }
+            con.marcar_justificacion(est_asis, id_asistencia, id_decano, dataGridView1);
+            conectar.Close();
         }
 
         private void btn_nojustificado_Click(object sender, EventArgs e)
         {
-            if (string.IsNullOrEmpty(txt_idasis.Text))
-            {
-                MessageBox.Show("Escoja una registro para marcar la Justificación");
-            }
-            else
-            {
-                string est_asis = "N";
-                int id_asistencia = Convert.ToInt32(txt_idasis.Text);
-                int id_decano = Convert.ToInt32(txt_usu_decano.Text);
+            string est_asis = "N";
+            int id_asistencia = Convert.ToInt32(txt_idasis.Text);
+            int id_decano = Convert.ToInt32(txt_usu_decano.Text);
 
-                con.marcar_justificacion(est_asis, id_asistencia, id_decano, dataGridView1);
-                conectar.Close();
+            conectar.ConnectionString = server;
+            conectar.Open();
+
+
+            SqlCommand cmd = new SqlCommand("sp_justificacion ", conectar);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@estado_justificacion", est_asis);
+            cmd.Parameters.AddWithValue("@id_asistencia", id_asistencia);
+            cmd.Parameters.AddWithValue("@id_decano", id_decano);
+
+            try
+            {
+                cmd.ExecuteNonQuery();
             }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+                throw;
+            }
+            con.marcar_justificacion(est_asis, id_asistencia, id_decano, dataGridView1);
+            conectar.Close();
         }
 
         private void btn_recargar_Click(object sender, EventArgs e)
@@ -109,7 +250,7 @@ namespace Proyecto_DesarrolloSoftware
 
             if (string.IsNullOrEmpty(txt_busqueda.Text))
             {
-                MessageBox.Show("La busqueda no puede quedar vacia");
+                MessageBox.Show("la busqueda no puede quedar vacia");
             }
             else
             {
@@ -117,11 +258,37 @@ namespace Proyecto_DesarrolloSoftware
                 {
                     MessageBox.Show("Seleccione un filtro antes de iniciar la busqueda");
                 }
-                else if (cmb_filtro.SelectedIndex == 0)
+                else if(cmb_filtro.SelectedIndex == 0)
                 {
-                    con.busqueda_decano_clase(id, busqueda, dataGridView1);
+                    conectar.ConnectionString = server;
+                    conectar.Open();
+
+                    SqlDataAdapter adapter = new SqlDataAdapter();
+                    DataTable contenedor = new DataTable();
+                    SqlCommand cmd = new SqlCommand("sp_bus_decano", conectar);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@clase", busqueda);
+                    cmd.Parameters.AddWithValue("@id_decano", id);
+
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                        adapter.SelectCommand = cmd;
+                        adapter.Fill(contenedor);
+                        dataGridView1.DataSource = contenedor;
+                    }
+                    catch (SqlException ex)
+                    {
+                        MessageBox.Show(ex.ToString());
+                        throw;
+                    }
+                    conectar.Close();
                 }
+
+                
             }
+
+                
         }
 
         private void iconButton1_Click(object sender, EventArgs e)
