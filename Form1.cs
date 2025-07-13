c79531faf5472f3e2ff1ed0a8ab164daee24a402
Armando Villamil <119384839+Arvillax@users.noreply.github.com>
2025-07-12 22:15:28 -0600

    Formulario Principal se paso

--- Form1.cs
+++ Form1.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 
 namespace Proyecto_DesarrolloSoftware
 {
-    
+
 
     public partial class Form1 : Form
     {
@@ -20,11 +20,117 @@ namespace Proyecto_DesarrolloSoftware
         public Form1()
         {
             InitializeComponent();
-            btnMaximizar.Text = "🗖";
+            // fijar pantalla
+            this.MinimumSize = new Size(1130, 761);
+            this.FormBorderStyle = FormBorderStyle.Sizable;
+            this.Resize += Form1_Resize;
         }
 
+        private void Form1_Resize(object sender, EventArgs e)// forzar que la aventana conserve el tamaño
+        {
+            if (this.Width < 1130)
+                this.Width = 1130;
 
-        private void Ingresar()
+            if (this.Height < 761)
+                this.Height = 761;
+        }
+        private bool contraseñaVisible = false;
+
+        private void btnMostrarContraseña_Click(object sender, EventArgs e)
+        {
+            contraseñaVisible = !contraseñaVisible;
+
+            if (contraseñaVisible)
+            {
+                txtContraseña.PasswordChar = '\0'; // Mostrar texto normal
+                btnMostrarContraseña.Text = "🙈";
+            }
+            else
+            {
+                txtContraseña.PasswordChar = '*'; // Ocultar con asteriscos
+                btnMostrarContraseña.Text = "👁️";
+            }
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrWhiteSpace(txtUsuario.Text) || string.IsNullOrWhiteSpace(txtContraseña.Text))
+            {
+                MessageBox.Show("Ingrese usuario y contraseña.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!int.TryParse(txtUsuario.Text.Trim(), out int idUsuario))
+            {
+                MessageBox.Show("El usuario debe ser un número válido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string contraseña = txtContraseña.Text.Trim();
+            clsConexion conexion = new clsConexion();
+            string tipoUsuario = conexion.ValidarUsuario(idUsuario, contraseña);
+
+            switch (tipoUsuario)
+            {
+                case "ADMINISTRADOR":
+                    frmGestion_Usuarios adminForm = new frmGestion_Usuarios();
+                    adminForm.Show();
+                    this.Hide();
+                    break;
+
+                case "SUPERVISOR":
+
+                    frmSupervisor supervisorForm = new frmSupervisor();
+                    supervisorForm.Show();
+                    this.Hide();
+                    break;
+
+                case "DOCENTE":
+                    int numero = Convert.ToInt32(txtUsuario.Text);
+                    frmDocente docenteForm = new frmDocente(numero);
+
+                    docenteForm.Show();
+                    this.Hide();
+                    break;
+
+                case "DECANO":
+                    int usuario_decano = Convert.ToInt32(txtUsuario.Text);
+
+                    frmDecano decanoForm = new frmDecano(usuario_decano);
+                    decanoForm.Show();
+                    this.Hide();
+                    break;
+
+                default:
+                    MessageBox.Show("Usuario o contraseña incorrectos o usuario inactivo.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+            }
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void lnContraseña_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            frmRecuperacion_De_Contraseñas frmRecuC = new frmRecuperacion_De_Contraseñas();
+            frmRecuC.Show();
+            this.Hide();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //
+        }
+
+        private void btn_cerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_ingresar_Click(object sender, EventArgs e)
         {
             if (string.IsNullOrWhiteSpace(txtUsuario.Text) || string.IsNullOrWhiteSpace(txtContraseña.Text))
             {
@@ -45,25 +151,30 @@ namespace Proyecto_DesarrolloSoftware
             switch (tipoUsuario)
             {
                 case "ADMINISTRADOR":
-                    frm_Admin_Principal adminForm = new frm_Admin_Principal();
+                    frmGestion_Usuarios adminForm = new frmGestion_Usuarios();
                     adminForm.Show();
                     this.Hide();
                     break;
 
                 case "SUPERVISOR":
+
                     frmSupervisor supervisorForm = new frmSupervisor();
                     supervisorForm.Show();
                     this.Hide();
                     break;
 
                 case "DOCENTE":
-                    frmDocente docenteForm = new frmDocente(idUsuario);
+                    int numero = Convert.ToInt32(txtUsuario.Text);
+                    frmDocente docenteForm = new frmDocente(numero);
+
                     docenteForm.Show();
                     this.Hide();
                     break;
 
                 case "DECANO":
-                    frmDecano decanoForm = new frmDecano(idUsuario);
+                    int usuario_decano = Convert.ToInt32(txtUsuario.Text);
+
+                    frmDecano decanoForm = new frmDecano(usuario_decano);
                     decanoForm.Show();
                     this.Hide();
                     break;
@@ -74,71 +185,9 @@ namespace Proyecto_DesarrolloSoftware
             }
         }
 
-
-        private void lnContraseña_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            frmRecuperacion_De_Contraseñas frmRecuC = new frmRecuperacion_De_Contraseñas();
-            frmRecuC.Show();
-            this.Hide();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            this.MinimumSize = new Size(800, 600);
-        }
-
-        private void btn_cerrar_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
-        }
-
-        private void btn_ingresar_Click(object sender, EventArgs e)
-        {
-            Ingresar();
-        }
-
         private void txtUsuario_KeyPress(object sender, KeyPressEventArgs e)
         {
             vali.solonumeros(e);
-        }
-
-        private void btnMinimizar_Click(object sender, EventArgs e)
-        {
-            this.WindowState = FormWindowState.Minimized;
-        }
-
-        private void btnMaximizar_Click(object sender, EventArgs e)
-        {
-
-            if (this.WindowState == FormWindowState.Maximized)
-            {
-                this.WindowState = FormWindowState.Normal;
-                this.Size = new Size(800, 600);
-                btnMaximizar.Text = "🗖"; 
-            }
-            else
-            {
-                this.WindowState = FormWindowState.Maximized;
-                btnMaximizar.Text = "🗗"; 
-            }
-        }
-
-        private void txtUsuario_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Enter)
-            {
-                Ingresar();
-                e.SuppressKeyPress = true;
-            }
-        }
-
-        private void txtContraseña_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Enter)
-            {
-                Ingresar();
-                e.SuppressKeyPress = true;
-            }
         }
     }
 }
